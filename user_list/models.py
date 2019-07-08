a2a9df7def1e5ccc1fb956ed0d32574a8b23eca1
Duxa98 <Duxa98@yandex.ru>
2019-07-08 19:41:41 +0300

    add models

--- user_list/models.py
+++ user_list/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+FOOD_PREFERENCES = ['menu1', 'menu2', 'menu3']
+
+class AppUser(models.Model):
+    user_id = models.IntegerField(primary_key=True)
+    login = models.CharField(unique=True)
+    weigth = models.FloatField()
+    birthday = models.DateField()
+    creation_time = models.DateTimeField()
+    update_time = models.DateTimeField()
+    food_preferences = models.CharField(choices=FOOD_PREFERENCES)
+
+    def __str__(self):
+        return f'{self.login}, {self.food_preferences}'
+
+
+class tokens(models.Model):
+    token_id = models.IntegerField(primary_key=True)
+    user_id = models.ForeignKey(to=AppUser, on_delete=models.CASCADE, related_name='user_token')
+    token = models.CharField(null=False)
