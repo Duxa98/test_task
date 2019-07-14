31e87b93b233cbef60fcce231813c4394ce945e9
Duxa98 <Duxa98@yandex.ru>
2019-07-14 16:32:10 +0300

    fix lint

--- test_task/urls.py
+++ test_task/urls.py
@@ -13,11 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
-
-from user_list.views import user_del, user_modify, user_info, users_list, user_create
 
 urlpatterns = [
     path('users/', include('user_list.urls')),
--- user_list/tests/test_apiviews.py
+++ user_list/tests/test_apiviews.py
@@ -6,9 +6,9 @@ from rest_framework.reverse import reverse
 
 from user_list.models import AppUser
 
+
 @pytest.fixture(scope='function', params=[1, 2])
 def django_client_setup(db, request):
-
     for i in range(request.param):
         AppUser(login=f'login_{i}', weight=i * 10).save()
 
@@ -30,4 +30,3 @@ def test_apiviews(django_client_setup):
     for i in range(param):
         assert ans[i]['login'] == f'login_{i}'
         assert ans[i]['weight'] == i * 10
-
